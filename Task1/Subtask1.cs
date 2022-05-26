b07f697147c74b0ddc6a36f06d29c7ef3901840d
bortnik470 <bortnik470a@gmail.com>
2022-05-26 12:29:55 +0300

    Add task1

---
+++ Task1/Subtask1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Course.Task1
+{
+    class Product
+    {
+        private string name;
+        private int price;
+        private int weight;
+
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+        public int Weight { get => weight; set => weight = value; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, int price, int weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Product)) return false;
+            return (this.Name == ((Product)obj).Name && this.Price == ((Product)obj).Price && this.Weight == ((Product)obj).Weight);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\tPrice: {Price}\tWeight: {Weight}";
+        }
+    }
+}
---
+++ Task1/Subtask2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Course.Task1
+{
+    class Buy
+    {
+        private Product[] products;
+
+        public Buy(params Product[] products)
+        {
+            this.products = products != null ? products : null;
+        }
+
+        public int GetNumberOfProducts()
+        {
+            return products.Length;
+        }
+
+        public int GetPrice()
+        {
+            int result = 0;
+            foreach(Product i in products)
+            {
+                result += i.Price;
+            }
+            return result;
+        }
+
+        public int GetWeight()
+        {
+            int result = 0;
+            foreach (Product i in products)
+            {
+                result += i.Weight;
+            }
+            return result;
+        }
+
+        public Product this[int index]
+        {
+            get
+            {
+                if (index > products.Length && index < 0)
+                    throw new ArgumentOutOfRangeException();
+                return products[index];
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            foreach(Product i in products)
+            {
+                result += i + "\n";
+            }
+
+            return result;
+        }
+    }
+}
